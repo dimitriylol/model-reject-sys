75d98796ffa01ed0c6861087ea59409d04071b43
Dmytro Lyman <liman.dimitriy@gmail.com>
2016-12-14 08:55:09 +0200

    it works! only via cider

--- src/model_reject_sys/state_vector/generate.clj
+++ src/model_reject_sys/state_vector/generate.clj
@@ -24,26 +24,30 @@
                                                      (fails-statistic model
                                                                       state-vector))))))
           (result-state-applying 0 (fails-statistic model))
-          (state-generator (count (elements model)))))
+          (state-generator)))
 
 (defn dump-res [{res-prob :ok-probability fails :fails}]
   (spit "res.log"
         (str "Probability result " res-prob "\n failed modules " fails "\n")
         :append true))
 
-(defn execute-tests [model] 
-  (map (fn [[multiplicity required-percentile correction]]
-         (println "Execute test with " multiplicity " multiplicity")
-         (dump-res
-          (update
-           (execute-test model
-                         (fn [n]
-                           (if (= 100 required-percentile)
-                             (n-state-vectors multiplicity n)
-                             (n-state-vectors multiplicity n required-percentile))))
-           :ok-probability
-           (partial * correction))))
+(defn execute-tests [model]
+  (let [n (count (elements model))
+        probability-all-works (probability model (first (n-state-vectors 0 n)))]
+    (map (fn [[multiplicity required-percentile correction]]
+           (println "Execute test with " multiplicity " multiplicity")
+           (dump-res
+            (update
+             (execute-test model
+                           (fn []
+                             (if (= 100 required-percentile)
+                               (n-state-vectors multiplicity n)
+                               (n-state-vectors multiplicity n required-percentile))))
+             :ok-probability
+             (fn [working-probability]
+               (+ probability-all-works
+                  (* correction working-probability))))))
        '((1 100 1)
          (2 100 1)
          (3 50 2)
-         (4 10 10))))
+         (4 10 10)))))
--- src/model_reject_sys/tasks.clj
+++ src/model_reject_sys/tasks.clj
@@ -1,11 +1,14 @@
 (ns model-reject-sys.tasks)
 
+(defn build-elems-state [interesting-elements state-map]
+  (apply hash-map
+         (mapcat (fn [elem-name]
+                   (list elem-name (elem-name state-map)))
+                 interesting-elements)))
+
 (defn task1 [state-map]
-  (let [interesting-elements '(:d1 :d2 :c1 :b1 :b2 :b3 :pr1 :pr2 :pr3 :a1 :a2 :m1)
-        elems-state (apply hash-map
-                           (mapcat (fn [elem-name]
-                                     (list elem-name (elem-name state-map)))
-                                   interesting-elements))]
+  (let [interesting-elements '(:d1 :d2 :c1 :b1 :b2 :b3 :pr1 :pr2 :pr3 :a1 :a2 :m1 :pr6)
+        elems-state (build-elems-state interesting-elements state-map)]
     (and (:d1 elems-state)
          (:d2 elems-state)
          (:c1 elems-state)
@@ -17,11 +20,68 @@
                   (:m1 elems-state)
                   (:a2 elems-state)
                   (:b3 elems-state)
-                  (:pr3 elems-state))))))
+                  (or (:pr3 elems-state)
+                      (:pr6 elems-state)))))))
 
-(defn task2 [state-vector] true)
-(defn task3 [state-vector] true)
-(defn task4 [state-vector] true)
+(defn task2 [state-map]
+  (let [interesting-elements '(:d8 :c6 :b3 :pr3 :pr6 :a2 :m1 :a1 :b1 :b2 :pr1 :pr2)
+        elems-state (build-elems-state interesting-elements state-map)]
+    (and (:d8 elems-state)
+         (:c6 elems-state)
+         (or (:pr3 elems-state)
+             (:pr6 elems-state))
+         (:b3 elems-state)         
+         (:a2 elems-state)
+         (:m1 elems-state)
+         (:a1 elems-state)
+         (or (:pr1 elems-state)
+             (:pr2 elems-state))
+         (or (:b1 elems-state)
+             (:b2 elems-state)))))
+
+(defn task3 [state-map]
+  (let [interesting-elements '(:d2 :d3 :c2 :b1 :b2 :pr1 :pr2 :a1 :m1 :a2 :b3 :pr6 :pr3)
+        elems-state (build-elems-state interesting-elements state-map)]
+    (and (:d2 elems-state)
+         (:d3 elems-state)
+         (:c2 elems-state)
+         (or (:b1 elems-state)
+             (:b2 elems-state))
+         (or (:pr1 elems-state)
+             (:pr2 elems-state))
+         (:a1 elems-state)         
+         (:m1 elems-state)
+         (:a2 elems-state)
+         (:b3 elems-state)
+         (or (:pr6 elems-state)
+             (:pr3 elems-state)))))
+
+(defn task4 [state-map]
+  (let [interesting-elements '(:d6 :d7 :c5 :pr3 :pr6 :a2 :b3 :m1 :a1 :b1 :b2 :pr1 :pr2 :c4)
+        elems-state (build-elems-state interesting-elements state-map)]
+    (or
+     (and (:d6 elems-state)
+          (:d7 elems-state)
+          (:c5 elems-state)
+          (:b3 elems-state)
+          (or (:pr3 elems-state)
+              (:pr6 elems-state))
+          (:a2 elems-state)         
+          (:m1 elems-state)
+          (:a1 elems-state)
+          (or (:b1 elems-state)
+              (:b2 elems-state))
+          (or (:pr1 elems-state)
+              (:pr2 elems-state)))
+     (and (:d6 elems-state)
+          (:c4 elems-state)
+          (:m1 elems-state)
+          (:a1 elems-state)
+          (or (:pr1 elems-state)
+              (:pr2 elems-state))
+          (or (:b1 elems-state)
+              (:b2 elems-state))))))
 
 (defn tasks [elements-map]
-  (every? true? (map (fn [task] (task elements-map)) (list task1 task2 task3 task4))))
+  (every? true? (map (fn [task] (task elements-map))
+                     (list task1 task2 task3 task4))))
